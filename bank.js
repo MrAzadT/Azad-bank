378da79c7188513b06711e6f7084f8ac11c66fac
azadtamal <mrazadt@gmail.com>
2021-08-15 23:05:21 +0600

    done

--- bank.js
+++ bank.js
@@ -19,6 +19,31 @@ document
     const balanceTotal = document.getElementById("balanceTotal");
     const previousBalance = balanceTotal.innerText;
     const previousBalance2 = parseFloat(previousBalance);
-    const total = previousBalance2 + newDepositAmount2;
-    balanceTotal.innerText = total;
+    const totalD = previousBalance2 + newDepositAmount2;
+    balanceTotal.innerText = totalD;
+  });
+
+document
+  .getElementById("withdraw-button")
+  .addEventListener("click", function () {
+    const withdrawInput = document.getElementById("withdraw-input");
+    const withdrawInputAmount = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawInputAmount);
+
+    const totalWithdraw = document.getElementById("totalWithdraw");
+    const withdrawAmount = totalWithdraw.innerText;
+    const newTotalWithdraw = parseFloat(withdrawAmount);
+
+    const newWithdraw = newWithdrawAmount + newTotalWithdraw;
+
+    totalWithdraw.innerText = newWithdraw;
+
+    // ========
+    const balanceTotal = document.getElementById("balanceTotal");
+    const previousBalance = balanceTotal.innerText;
+    const previousBalance2 = parseFloat(previousBalance);
+    const totalB = previousBalance2 - newWithdrawAmount;
+    balanceTotal.innerText = totalB;
+
+    withdrawInput.value = "";
   });
