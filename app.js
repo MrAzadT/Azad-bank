8f9833078b6b79cffbcc6d1cbe18ab7b16fa7f21
azadtamal <mrazadt@gmail.com>
2021-08-15 16:02:44 +0600

    bank-web

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById("loginBtn").addEventListener("click", function () {
+  const gmailInput = document.getElementById("gmailInput");
+  const userGmail = gmailInput.value;
+  console.log(userGmail);
+  const PassInput = document.getElementById("PassInput");
+  const userPass = PassInput.value;
+  console.log(userPass);
+});
