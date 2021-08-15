7646f6a15156f2ca3dbdf286e98e4d95b6581710
azadtamal <mrazadt@gmail.com>
2021-08-15 21:31:12 +0600

    Deposit

--- bank.js
+++ bank.js
@@ -2,8 +2,17 @@ document
   .getElementById("deposit-button")
   .addEventListener("click", function () {
     const depositInput = document.getElementById("deposit-input");
-    const depositAmount = depositInput.value;
+    const newDepositAmount = depositInput.value;
+    const newDepositAmount2 = parseFloat(newDepositAmount);
+
     const depositTotal = document.getElementById("depositTotal");
-    depositTotal.innerText = depositAmount;
+    // depositTotal.innerText = depositAmount;
+
+    const previousDeposit = depositTotal.innerText;
+    const previousDeposit2 = parseFloat(previousDeposit);
+
+    const newDeposit = previousDeposit2 + newDepositAmount2;
+    depositTotal.innerText = newDeposit;
+
     depositInput.value = "";
   });
